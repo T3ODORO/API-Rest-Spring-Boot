8a34f394e909d034d0413727b59ae1f14c992fa9
T3ODORO <79917308+T3ODORO@users.noreply.github.com>
2024-11-15 10:39:24 -0300

    finalizado

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -19,10 +19,9 @@ public class Medico {
     private Long id;
     private String nome;
     private String email;
-
     private String telefone;
-
     private String crm;
+    private Boolean ativo;
 
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
@@ -31,6 +30,7 @@ public class Medico {
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -38,4 +38,22 @@ public class Medico {
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
     }
+
+    public void excluir() {
+        this.ativo = false;
+    }
+
+    public void atualizarInformacao(DadosAtualizarMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+
 }
